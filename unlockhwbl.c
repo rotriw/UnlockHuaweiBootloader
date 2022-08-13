198cabb054b1be84f404a929cd23562b308a27f1
hg56th56gd6g <61111700+hg56th56gd6g@users.noreply.github.com>
2022-08-13 09:25:33 +0800

    Add files via upload

--- unlockhwbl.c
+++ unlockhwbl.c
@@ -12,77 +12,81 @@ void main(void){
         //unicode结尾两个NULL
         0
     };
-    //获取stdout句柄
-    HANDLE stdo;
-    if((stdo=GetStdHandle(STD_OUTPUT_HANDLE))==INVALID_HANDLE_VALUE){ExitProcess(0);}
-    //hg56th56gd6g
+    HANDLE stdo,heap,wrrd;
     DWORD temp;
+    void *stat,*fpas,*dpas;
+    STARTUPINFOW StartUpInfo;
+    PROCESS_INFORMATION ProcessInformation;
+    //获取stdout句柄
+    if((stdo=GetStdHandle(STD_OUTPUT_HANDLE))==INVALID_HANDLE_VALUE){goto exit;}
+    //hg56th56gd6g
     WriteFile(stdo,"BuildBy hg56th56gd6g\n",21,&temp,0);
-    //获取进程默认heap
-    HANDLE heap;
-    if(!(heap=GetProcessHeap())){
-        WriteFile(stdo,"GetProcessHeapErr",17,&temp,0);
-        ExitProcess(0);
+    //获取进程默认heap,给createpipe需要的临时参数分配内存
+    if(!(
+        (heap=GetProcessHeap())&&
+        (stat=HeapAlloc(heap,0,sizeof(SECURITY_ATTRIBUTES)))
+    )){
+        WriteFile(stdo,"HeapErr",7,&temp,0);
+        goto exit;
     }
     //创建管道用于fastboot.exe的stdo,2个句柄(stdo的io句柄)
-    void *stat;
-    if(!(stat=HeapAlloc(heap,0,sizeof(SECURITY_ATTRIBUTES)))){
-        WriteFile(stdo,"HeapAllocErr",12,&temp,0);
-        ExitProcess(0);
-    }
     ((SECURITY_ATTRIBUTES *)stat)->nLength=sizeof(SECURITY_ATTRIBUTES);
     ((SECURITY_ATTRIBUTES *)stat)->lpSecurityDescriptor=0;
     ((SECURITY_ATTRIBUTES *)stat)->bInheritHandle=TRUE;
-    HANDLE wrrd,wrwr;
-    if(!(CreatePipe(&wrrd,&wrwr,(SECURITY_ATTRIBUTES *)stat,0))){
+    if(!(CreatePipe(&wrrd,&(StartUpInfo.hStdOutput),(SECURITY_ATTRIBUTES *)stat,0))){
         WriteFile(stdo,"CreatePipeErr",11,&temp,0);
-        ExitProcess(0);
+        goto exit;
     }
+    HeapFree(heap,0,stat);
     //设置子进程可以继承stdout句柄,且被调用CloseHandle时不关闭对象句柄
-    if(!SetHandleInformation(wrwr,HANDLE_FLAG_INHERIT|HANDLE_FLAG_PROTECT_FROM_CLOSE,HANDLE_FLAG_INHERIT|HANDLE_FLAG_PROTECT_FROM_CLOSE)){
+    if(!SetHandleInformation(StartUpInfo.hStdOutput,HANDLE_FLAG_INHERIT|HANDLE_FLAG_PROTECT_FROM_CLOSE,HANDLE_FLAG_INHERIT|HANDLE_FLAG_PROTECT_FROM_CLOSE)){
         WriteFile(stdo,"SetHandleInfoErr",14,&temp,0);
-        ExitProcess(0);
+        goto exit;
     }
-    //获取platform-tools\fastboot.exe完整路径,这是为了加快CreateProcessW寻找可执行文件
-    void *fpas,*dpas;
+    //填充startupinfo
+    StartUpInfo.cb=sizeof(STARTUPINFOW);
+    //10个null
+    StartUpInfo.lpReserved=0;
+    StartUpInfo.lpDesktop=0;
+    StartUpInfo.lpTitle=0;
+    StartUpInfo.dwX=0;
+    StartUpInfo.dwY=0;
+    StartUpInfo.dwXSize=0;
+    StartUpInfo.dwYSize=0;
+    StartUpInfo.dwXCountChars=0;
+    StartUpInfo.dwYCountChars=0;
+    StartUpInfo.dwFillAttribute=0;
+    //dwFlags
+    StartUpInfo.dwFlags=STARTF_USESTDHANDLES;
+    //3个null
+    StartUpInfo.wShowWindow=0;
+    StartUpInfo.cbReserved2=0;
+    StartUpInfo.lpReserved2=0;
+    //stdi
+    StartUpInfo.hStdInput=0;
+    //stdo
+    StartUpInfo.hStdError=StartUpInfo.hStdOutput;
+    //获取platform-tools和platform-tools\fastboot.exe完整路径,这是为了加快CreateProcessW处理参数
     if(!(
+        (temp=GetFullPathNameW(L"platform-tools",0,dpas,0))&&
+        (dpas=HeapAlloc(heap,0,(SIZE_T)(temp<<1)))&&
+        (GetFullPathNameW(L"platform-tools",temp,dpas,0))&&
         (temp=GetFullPathNameW(L"platform-tools\\fastboot.exe",0,fpas,0))&&
         (fpas=HeapAlloc(heap,0,(SIZE_T)(temp<<1)))&&
         (GetFullPathNameW(L"platform-tools\\fastboot.exe",temp,fpas,0))
     )){
-        WriteFile(stdo,"Get\"platform-tools\\fastboot.exe\"FullPathErr",43,&temp,0);
-        ExitProcess(0);
+        WriteFile(stdo,"GetFullPathErr",14,&temp,0);
+        goto exit;
     }
-    //获取platform-tools完整路径作为fastboot.exe运行目录
-    if(!(
-        (temp=GetFullPathNameW(L"platform-tools",0,dpas,0))&&
-        (dpas=HeapAlloc(heap,0,(SIZE_T)(temp<<1)))&&
-        (GetFullPathNameW(L"platform-tools",temp,dpas,0))
-    )){
-        WriteFile(stdo,"Get\"platform-tools\"FullPathErr",30,&temp,0);
-        ExitProcess(0);
+    //调用CodeInit
+    if(!(stat=CodeInit(&(line.code)))){
+        WriteFile(stdo,"CodeInitErr",11,&temp,0);
+        goto exit;
     }
-    stat=CodeInit(&(line.code));
     //状态,用于判断已经读完"OKAY"的哪个字符了;缓冲区,由基地址,当前指针,最大地址,缓冲区长度组成
     uint8_t data=0,*base=0,*pnow,*pmax;
     DWORD blen=0;
     //循环调用CreateProcessW直到管道里读到"OKAY"
-    STARTUPINFOW StartUpInfo={
-        sizeof(STARTUPINFOW),
-        //10个null
-        0,0,0,0,0,0,0,0,0,0,
-        //dwFlags
-        STARTF_USESTDHANDLES,
-        //3个null
-        0,0,0,
-        //stdi
-        0,
-        //stdo
-        wrwr,
-        //stde
-        wrwr,
-    };
-    PROCESS_INFORMATION ProcessInformation;
     for(;;){
         //创建进程
         if(!CreateProcessW(fpas,(LPWSTR)&line,0,0,TRUE,0,0,dpas,&StartUpInfo,&ProcessInformation)){
@@ -193,5 +197,6 @@ void main(void){
     //调用CodeExit
     ends:
     CodeExit(stat);
+    exit:
     ExitProcess(0);
 }
