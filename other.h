e0dcb818e7e1e6de001afc752a95bbc0ef2f1cd1
hg56th56gd6g <61111700+hg56th56gd6g@users.noreply.github.com>
2022-09-24 12:41:48 +0800

    Delete other.h

--- other.h
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-#include <stdint.h>
-#define DLLImport __declspec(dllimport)
-#define DLLExport __declspec(dllexport)
-typedef struct{
-    uint8_t head[22];
-    uint16_t code[16];
-    uint16_t zero;
-}CmdLine;
